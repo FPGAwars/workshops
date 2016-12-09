fff2fc0981c8b23fccb556d65321569084446e2b
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2016-12-09 18:53:26 +0100

    Add OurenseMakersLab workshop

---
+++ 2016-12-10-OurenseMakersLab/README.md
@@ -0,0 +1,14 @@
+![]()
+
+# OurenseMakersLab workshop
+
+2016-12-10
+
+## Documentation
+
+[Find more information in the wiki]() (In Spanish)
+
+## License
+
+![](https://github.com/FPGAwars/workshops/raw/master/wiki/attribution-share-alike-creative-commons-license.png)
+[Creative Commons Attribution-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-sa/4.0/)
---
+++ 2016-12-10-OurenseMakersLab/examples/counter/counter_tb.v
@@ -0,0 +1,39 @@
+//-- Testbench
+
+`default_nettype none
+`timescale 100 ns / 10 ns
+`define DUMPSTR(x) `"x.vcd`"
+
+
+module counter_tb();
+
+//-- Simulation time: 1us (10 * 100ns)
+parameter DURATION = 10;
+
+//-- Clock signal. It is not used in this simulation
+reg clk = 0;
+always #0.5 clk = ~clk;
+
+//-- Leds port
+wire [4:0] leds;
+
+//-- Counter bits length
+localparam N = 6;
+
+counter #(
+           .N(N)
+)  CONT0 (
+           .clk(clk),
+           .leds(leds)
+);
+
+initial begin
+  //-- File where to store the simulation
+  $dumpfile(`DUMPSTR(`VCD_OUTPUT));
+  $dumpvars(0, counter_tb);
+
+  #(DURATION) $display("END of the simulation");
+  $finish;
+end
+
+endmodule
